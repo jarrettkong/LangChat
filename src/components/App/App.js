6879c3470e945945a92dbd19ea35ea7f5513dfc2
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-16 17:51:18 -0600

    Splash page for user sign up/ login

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,13 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducers from "../../reducers";
+import SplashPage from "../SplashPage/SplashPage";
 
 const App = () => {
   return (
     <Provider store={createStore(reducers)}>
       <View style={styles.container}>
-        <Text>App component</Text>
+        <SplashPage />
       </View>
     </Provider>
   );
