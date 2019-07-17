549190862160908e18a84db59cd77dcbb2bd080e
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-17 10:02:30 -0600

    Split signup component to make it easier for user

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -5,39 +5,58 @@ import { Actions } from 'react-native-router-flux';
 import { MaterialCommunityIcons, AntDesign, EvilIcons, Feather, Ionicons } from '@expo/vector-icons';
 
 class SignUpForm extends Component {
+	state = { userInfo: true };
+
+	buttonToRender = () => {
+		if (this.state.userInfo) {
+			return <Button onPress={() => this.setState({ userInfo: false })}>Next</Button>;
+    }
+    return <Button>Sign Up!</Button>
+	};
+
 	render () {
 		const { containerStyle, inputContainerStyle, textHeaderStyle, buttonContainerStyle } = styles;
 		return (
 			<View style={containerStyle}>
 				<EvilIcons name="close" size={40} onPress={() => Actions.splashPage()} style={{ width: '13%' }} />
 				<Text style={textHeaderStyle}>Sign Up</Text>
-				<View style={inputContainerStyle}>
-					<Input label={<AntDesign name="user" size={30} color="#999" />} placeholder="Username" />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input label={<Feather name="plus" size={30} color="#999" />} placeholder="First Name" />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input label={<Feather name="plus" size={30} color="#999" />} placeholder="Last Name" />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input label={<Ionicons name="md-globe" size={30} color="#999" />} placeholder="Country" />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input label={<MaterialCommunityIcons name="email-outline" size={30} color="#999" />} placeholder="Email" />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input label={<AntDesign name="lock" size={30} color="#999" />} placeholder="Password" secureTextEntry />
-				</View>
-				<View style={inputContainerStyle}>
-					<Input
-						label={<AntDesign name="lock" size={30} color="#999" />}
-						placeholder="Confirm Password"
-						secureTextEntry
-					/>
-				</View>
+				{this.state.userInfo ? (
+					<React.Fragment>
+						<View style={inputContainerStyle}>
+							<Input label={<Feather name="plus" size={30} color="#999" />} placeholder="First Name" />
+						</View>
+						<View style={inputContainerStyle}>
+							<Input label={<Feather name="plus" size={30} color="#999" />} placeholder="Last Name" />
+						</View>
+						<View style={inputContainerStyle}>
+							<Input label={<AntDesign name="user" size={30} color="#999" />} placeholder="Username" />
+						</View>
+						<View style={inputContainerStyle}>
+							<Input label={<Ionicons name="md-globe" size={30} color="#999" />} placeholder="Country" />
+						</View>
+					</React.Fragment>
+				):(
+					<React.Fragment>
+						<View style={inputContainerStyle}>
+							<Input
+								label={<MaterialCommunityIcons name="email-outline" size={30} color="#999" />}
+								placeholder="Email"
+							/>
+						</View>
+						<View style={inputContainerStyle}>
+							<Input label={<AntDesign name="lock" size={30} color="#999" />} placeholder="Password" secureTextEntry />
+						</View>
+						<View style={inputContainerStyle}>
+							<Input
+								label={<AntDesign name="lock" size={30} color="#999" />}
+								placeholder="Confirm Password"
+								secureTextEntry
+							/>
+						</View>
+					</React.Fragment>
+				)}
 				<View style={buttonContainerStyle}>
-					<Button>Sign Up!</Button>
+					{this.buttonToRender()}
 				</View>
 			</View>
 		);
