31e722254e70310f6de8d0f3ae27b1fa94d3709c
Max Silver <Maxbsilver@gmail.com>
2019-07-17 15:13:39 -0600

    add swiper

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
-
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import Tutorial from '../Tutorial/Tutorial';
+import Welcome from '../Welcome/Welcome';
 export default class Home extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+	render () {
+		return (
+			<View>
+				<Tutorial />
+			</View>
+		);
+	}
 }
-const styles = {
-
-};
+const styles = {};
--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,12 +1,44 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Swiper from 'react-native-web-swiper';
 
-export default class Turotial extends Component {
-    render() {
-        return (
-            <View>
-                <Text> textInComponent </Text>
-            </View>
-        )
-    }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1
+	},
+	slideContainer: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	slide1: {
+		backgroundColor: 'rgba(20,20,200,0.3)'
+	},
+	slide2: {
+		backgroundColor: 'rgba(20,200,20,0.3)'
+	},
+	slide3: {
+		backgroundColor: 'rgba(200,20,20,0.3)'
+	}
+});
+
+export default class Tutorial extends React.Component {
+	render () {
+		return (
+			<View style={styles.container}>
+                <Text>swiper</Text>
+				<Swiper>
+					<View style={[ styles.slideContainer, styles.slide1 ]}>
+						<Text>Slide 1</Text>
+					</View>
+					<View style={[ styles.slideContainer, styles.slide2 ]}>
+						<Text>Slide 2</Text>
+					</View>
+					<View style={[ styles.slideContainer, styles.slide3 ]}>
+						<Text>Slide 3</Text>
+					</View>
+				</Swiper>
+			</View>
+		);
+	}
 }
