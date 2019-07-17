dfb34d759c68965e998db048269d91ca03f7ebe6
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-17 15:20:43 -0600

    Register reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { authReducer } from './authReducer';
 import { messagesReducer } from './messagesReducer';
+import { registerReducer } from './RegisterReducer';
 
 export default combineReducers({
 	auth: authReducer,
-	messages: messagesReducer
+  messages: messagesReducer,
+  register: registerReducer
 });
