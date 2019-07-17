99f5be5b4d3e78c39954c1f5cdb0e3b2c509299f
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-17 05:47:10 -0600

    Add navigation to SignUpForm Component

---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SignUpForm extends Component {
+  render() {
+    return (
+      <View>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+        <Text> SignUpForm </Text>
+      </View>
+    );
+  }
+}
+
+export default SignUpForm;
