1917b66f3b2bee64cfa588660107fa91488ffd51
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-17 05:47:55 -0600

    Implement react native navigation

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Scene, Router } from 'react-native-router-flux';
+import SplashPage from "./components/SplashPage/SplashPage";
+import LoginForm from "./components/LoginForm/LoginForm";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+
+const RouterComponent = () => {
+	return (
+    <Router>
+      <Scene key="root">
+        <Scene key="splashPage" component={SplashPage} initial hideNavBar={true}/>
+        <Scene key="loginForm" component={LoginForm} hideNavBar={true}/>
+        <Scene key="signUpForm" component={SignUpForm} hideNavBar={true}/>
+      </Scene>
+    </Router>
+	);
+};
+
+export default RouterComponent;
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,25 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import reducers from "../../reducers";
-import SplashPage from "../SplashPage/SplashPage";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../../reducers';
+import Router from '../../Router';
 
 const App = () => {
-  return (
-    <Provider store={createStore(reducers)}>
-      <View style={styles.container}>
-        <SplashPage />
-      </View>
-    </Provider>
-  );
+	return (
+		<Provider store={createStore(reducers)}>
+				<Router />
+		</Provider>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
 
 export default App;
