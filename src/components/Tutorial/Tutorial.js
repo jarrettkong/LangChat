d45e91f79296ce23bb8b786c2cb53d0c15d0628e
Max Silver <Maxbsilver@gmail.com>
2019-07-21 15:10:59 -0600

    setup profile

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Swiper from 'react-native-web-swiper';
-import Drawer from '../NavDrawer/ NavDrawer';
 const styles = StyleSheet.create({
 	container: {
 		height: '100%',
