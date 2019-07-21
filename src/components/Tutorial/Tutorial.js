087d6259cc90a1a6c78a3c89328bf8a5035a00a3
Max Silver <Maxbsilver@gmail.com>
2019-07-21 15:11:41 -0600

    move tutorial styling to bottom of component

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Swiper from 'react-native-web-swiper';
+
+export default class Tutorial extends React.Component {
+	render () {
+		return (
+			<View style={styles.container}>
+				<Swiper>
+					<View style={[ styles.slideContainer, styles.slide1 ]}>
+						<Text>Slide 1</Text>
+					</View>
+					<View style={[ styles.slideContainer, styles.slide2 ]}>
+						<Text>Slide 2</Text>
+					</View>
+					<View style={[ styles.slideContainer, styles.slide3 ]}>
+						<Text>Slide 3</Text>
+					</View>
+				</Swiper>
+			</View>
+		);
+	}
+}
 const styles = StyleSheet.create({
 	container: {
 		height: '100%',
@@ -25,23 +45,3 @@ const styles = StyleSheet.create({
 		backgroundColor: 'rgba(200,20,20,0.3)'
 	}
 });
-
-export default class Tutorial extends React.Component {
-	render () {
-		return (
-			<View style={styles.container}>
-				<Swiper>
-					<View style={[ styles.slideContainer, styles.slide1 ]}>
-						<Text>Slide 1</Text>
-					</View>
-					<View style={[ styles.slideContainer, styles.slide2 ]}>
-						<Text>Slide 2</Text>
-					</View>
-					<View style={[ styles.slideContainer, styles.slide3 ]}>
-						<Text>Slide 3</Text>
-					</View>
-				</Swiper>
-			</View>
-		);
-	}
-}
