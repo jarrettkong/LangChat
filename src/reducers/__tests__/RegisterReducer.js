3a12b5cde6f18dd458206705c39cd261c9924647
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-21 23:57:58 -0600

    RegisterReducer fully tested

--- src/reducers/__tests__/RegisterReducer.js
+++ src/reducers/__tests__/RegisterReducer.js
@@ -7,4 +7,39 @@ describe("RegisterReducer", () => {
     const result = RegisterReducer(undefined, false);
     expect(result).toEqual(expected);
   });
+
+  it("should return state with an array of firstName", () => {
+    const expected = {"country": "United States", "email": "", "firstName": "Matthew", "lastName": "", "password": "", "userName": ""};
+    const result = RegisterReducer(undefined, actions.createFirstName("Matthew"));
+
+    expect(result).toEqual(expected);
+  });
+  
+  it("should return state with an array of lastName", () => {
+    const expected = {"country": "United States", "email": "", "firstName": "", "lastName": "Kaplan", "password": "", "userName": ""};
+    const result = RegisterReducer(undefined, actions.createLastName("Kaplan"));
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return state with an array of password", () => {
+    const expected = {"country": "United States", "email": "", "firstName": "", "lastName": "", "password": "mockPassword", "userName": ""};
+    const result = RegisterReducer(undefined, actions.createPassword("mockPassword"));
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return state with an array of userName", () => {
+    const expected = {"country": "United States", "email": "", "firstName": "", "lastName": "", "password": "", "userName": "mockUser"};
+    const result = RegisterReducer(undefined, actions.createUserName("mockUser"));
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return state with an array of userName", () => {
+    const expected = {"country": "United States", "email": "mockEmail", "firstName": "", "lastName": "", "password": "", "userName": ""};
+    const result = RegisterReducer(undefined, actions.createEmail("mockEmail"));
+
+    expect(result).toEqual(expected);
+  });
 });
