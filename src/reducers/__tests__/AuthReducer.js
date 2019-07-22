5bd0e424268aa2e06c9b5646b0e2fa24825c83bf
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-21 23:50:07 -0600

    AuthReducer testing

--- src/reducers/__tests__/AuthReducer.js
+++ src/reducers/__tests__/AuthReducer.js
@@ -7,4 +7,18 @@ describe("AuthReducer", () => {
     const result = AuthReducer(undefined, false);
     expect(result).toEqual(expected);
   });
+
+  it("should return state with an array of username", () => {
+    const expected = {"password": "", "username": "mockUser"};
+    const result = AuthReducer(undefined, actions.changeUsername("mockUser"));
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return state with an array of password", () => {
+    const expected = {"password": "mockPassword", "username": ""};
+    const result = AuthReducer(undefined, actions.changePassword("mockPassword"));
+
+    expect(result).toEqual(expected);
+  });
 });
