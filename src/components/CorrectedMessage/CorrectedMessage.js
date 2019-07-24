01e6587e0cd4c31cf452094114668375ca174432
Max Silver <Maxbsilver@gmail.com>
2019-07-24 12:24:38 -0600

    add proptypes to correctedmessage

--- src/components/CorrectedMessage/CorrectedMessage.js
+++ src/components/CorrectedMessage/CorrectedMessage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
-
+import PropTypes from 'prop-types';
 const CorrectedMessage = props => {
 	return (
 		<View>
@@ -16,5 +16,11 @@ const CorrectedMessage = props => {
 		</View>
 	);
 };
+
+CorrectedMessage.propTypes = {
+	reference: PropTypes.object,
+	message: PropTypes.string
+}
+
 
 export default CorrectedMessage;
