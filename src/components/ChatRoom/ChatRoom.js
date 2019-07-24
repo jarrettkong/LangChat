005744e019eb0ac74c58ae7db5473d6b1655a94a
Max Silver <Maxbsilver@gmail.com>
2019-07-24 12:22:12 -0600

    add proptypes to chatroom

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -6,6 +6,7 @@ import { Ionicons } from '@expo/vector-icons';
 import { addMessage, addExistingMessages } from '../../actions';
 import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import NavDrawer from '../NavDrawer/NavDrawer';
+import PropTypes from 'prop-types';
 
 export class ChatRoom extends Component {
 	constructor(props) {
