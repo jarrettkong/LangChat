490aa6f27bc9b830620dcf311d9faad58438000e
Max Silver <Maxbsilver@gmail.com>
2019-07-24 12:54:26 -0600

    finish proptypes

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Image } from 'react-native';
 import NavDrawer from '../NavDrawer/NavDrawer';
 import { connect } from 'react-redux';
-import { Actions } from 'react-native-router-flux';
+import PropTypes from 'prop-types';
+
 class Welcome extends Component {
 	render () {
 		return (
 			<View style={styles.container}>
 				<NavDrawer>
+					<Image style={styles.imageStyle} source={require('../../../assets/talking.jpg')} />
 					<Text style={styles.welcomeText}>
 						Welcome back <Text style={styles.offColor}>{this.props.user.username}</Text>!
 					</Text>
@@ -16,7 +18,9 @@ class Welcome extends Component {
 		);
 	}
 }
-
+Welcome.propTypes = {
+	user: PropTypes.object.isRequired
+};
 const styles = StyleSheet.create({
 	container: {
 		flex: 1.0
@@ -32,6 +36,11 @@ const styles = StyleSheet.create({
 		color: '#3B5998',
 		fontWeight: 'bold',
 		fontSize: 20
+	},
+	imageStyle: {
+		height: '50%',
+		width: '100%',
+		opacity: 0.8
 	}
 });
 export const mapStateToProps = state => ({
