bf62b8004e7fa5797d7303b61736a67a06f1a156
Matthew Kaplan <kaplan.matthew.p@gmail.com>
2019-07-26 05:43:32 -0600

    Spinner icon while tutorial loads

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -3,14 +3,30 @@ import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import Swiper from 'react-native-web-swiper';
 import { Actions } from 'react-native-router-flux';
 import { styles } from './styles';
+import Spinner from '../common/Spinner';
 
 export default class Tutorial extends React.Component {
+	state = { loading: true };
+
+	handleImageLoaded () {
+		this.setState({ loading: false });
+	}
+
 	render () {
 		return (
 			<SafeAreaView style={styles.container}>
 				<Swiper>
 					<View style={[ styles.slideContainer, styles.slide1 ]}>
-						<Image style={styles.imageStyle} source={{ uri: 'https://i.imgur.com/ODVGwz9.gif' }} />
+						<Image
+							style={styles.imageStyle}
+							onLoad={() => this.handleImageLoaded()}
+							source={{ uri: 'https://i.imgur.com/ODVGwz9.gif' }}
+						/>
+						{this.state.loading && (
+							<View style={{position: "absolute", top: 350}}>
+								<Spinner />
+							</View>
+						)}
 					</View>
 					<View style={[ styles.slideContainer, styles.slide2 ]}>
 						<Image style={styles.imageStyle} source={{ uri: 'https://i.imgur.com/sfw9qgf.gif' }} />
